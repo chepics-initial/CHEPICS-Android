efe1089a1112991b075708e5b8f2f51eb9e8fd6c
tatsuyoshi-kawajiri <tatsuyoshi-kawajiri@navitime.co.jp>
2024-09-12 18:09:42 +0900

    フォーム投稿の口を追加

--- app/src/main/java/com/chepics/chepics/feature/mypage/top/MyPageTopScreen.kt
+++ app/src/main/java/com/chepics/chepics/feature/mypage/top/MyPageTopScreen.kt
@@ -1,5 +1,7 @@
 package com.chepics.chepics.feature.mypage.top
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -24,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -136,8 +139,44 @@ fun MyPageTopScreen(navController: NavController, viewModel: MyPageTopViewModel 
                 )
             }
 
+            // TODO: - 不具合フォーム＆要望フォーム
+
+            val context = LocalContext.current
+
+            Spacer(modifier = Modifier.height(160.dp))
+
+            Text(
+                text = "不具合フォーム",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable {
+                        context.startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLSc8bindm6dfR0lbWCHcExOsHtQ4x1uIQPPHXs5HKOqCjaIhFQ/viewform?usp=sf_link")
+                            )
+                        )
+                    },
+                color = Color.Blue
+            )
+
+            Text(
+                text = "要望フォーム",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable {
+                        context.startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLSczgy8l1H0pgagpjR8d2S4xpAMr0SlRTL1e4MkWeQyh212ylg/viewform?usp=sf_link")
+                            )
+                        )
+                    },
+                color = Color.Blue
+            )
+
             // TODO: - デバッグ用なのであとで削除
-            Spacer(modifier = Modifier.height(80.dp))
+
             Text(text = "※同じメールアドレスで起動処理をデバッグするためのユーザー削除 ↓")
             TextButton(onClick = { viewModel.onTapDeleteButton() }) {
                 Text(
